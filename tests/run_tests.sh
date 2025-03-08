9baa441635f5605ecd75652eed057f574c938800
Logan <logand007@outlook.com>
2025-03-08 14:15:44 -0600

    reverted previous change and added headers to ./run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,6 +3,7 @@ total_fails=0
 testfiles=$(ls | grep -e ".*_test$")
 for test in $testfiles
 do
+    printf "\n\n----- RUNNING $test -----\n\n"
     ./$test 
     total_fails=$(($total_fails + $?)) 
 done
